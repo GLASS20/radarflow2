e7bec7b49f6a5725fc80c9c5b88df4d9b8374f48
Suka39 <97377511+GLASS20@users.noreply.github.com>
2024-07-22 21:05:35 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,31 +1,33 @@
 use std::error::Error;
+use std::time::Duration;
 
 use serde::{Deserialize, Serialize};
 use vergen::EmitBuilder;
+use reqwest::blocking::Client;
 
 #[derive(Clone, Deserialize, Serialize)]
 struct InfoJson {
     build_number: usize,
-    timestamp: String
+    timestamp: String,
 }
 
 fn download(url: &str, to: &str) -> Result<(), Box<dyn Error>> {
-    let content = reqwest::blocking::get(url)
-        .unwrap_or_else(|_| panic!("Downloading \"{to}\""))
-        .text()
-        .expect("Convert response to text");
+    let client = Client::builder()
+        .timeout(Duration::from_secs(10)) // 设置请求超时时间为10秒
+        .build()?;
 
-    std::fs::write(to, content)
-    .expect("Write to file");
+    let content = client.get(url).send()?.text()?;
+
+    std::fs::write(to, content)?;
 
     Ok(())
 }
 
 fn build_number() -> Result<(), Box<dyn Error>> {
-    let content = reqwest::blocking::get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/info.json")
-        .unwrap_or_else(|_| panic!("Downloading info.json"))
-        .text()
-        .expect("Convert response to text");
+    let content = Client::new()
+        .get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/info.json")
+        .send()?
+        .text()?;
 
     let info = serde_json::from_str::<InfoJson>(&content)?;
     println!("cargo:rustc-env=CS2_BUILD_NUMBER={}", info.build_number);
@@ -34,21 +36,20 @@ fn build_number() -> Result<(), Box<dyn Error>> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-
     download(
         "https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client.dll.rs",
-        "./src/dma/cs2dumper/client_mod.rs"
-    ).expect("Failed to download build file \"client.dll.rs\"");
+        "./src/dma/cs2dumper/client_mod.rs",
+    )?;
 
     download(
         "https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.rs",
-        "./src/dma/cs2dumper/offsets_mod.rs"
-    ).expect("Failed to download build file \"offsets.rs\"");
+        "./src/dma/cs2dumper/offsets_mod.rs",
+    )?;
 
     download(
         "https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/engine2.dll.rs",
-        "./src/dma/cs2dumper/engine2_mod.rs"
-    ).expect("Failed to download build file \"engine2.dll.rs\"");
+        "./src/dma/cs2dumper/engine2_mod.rs",
+    )?;
 
     build_number()?;
 
