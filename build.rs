b87957173560c01b9b34a3ff15c8c3e5f0034605
Suka39 <97377511+GLASS20@users.noreply.github.com>
2024-07-23 18:55:42 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,72 +1,57 @@
 use std::error::Error;
-use std::time::Duration;
 
 use serde::{Deserialize, Serialize};
 use vergen::EmitBuilder;
-use reqwest::blocking::Client;
 
 #[derive(Clone, Deserialize, Serialize)]
 struct InfoJson {
     build_number: usize,
-    timestamp: String,
+    timestamp: String
 }
 
 fn download(url: &str, to: &str) -> Result<(), Box<dyn Error>> {
-    let client = Client::builder()
-        .timeout(Duration::from_secs(10))
-        .build()?;
+    let content = reqwest::blocking::get(url)
+        .unwrap_or_else(|_| panic!("Downloading \"{to}\""))
+        .text()
+        .expect("Convert response to text");
 
-    match client.get(url).send() {
-        Ok(response) => {
-            let content = response.text()?;
-            std::fs::write(to, content)?;
-            Ok(())
-        }
-        Err(e) => {
-            eprintln!("Error downloading {}: {}", url, e);
-            Ok(())
-        }
-    }
-}
-
-
-fn build_number() -> Result<(), Box<dyn Error>> {
-    let content = Client::new()
-        .get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/info.json")
-        .send()?
-        .text()?;
-
-    let info = serde_json::from_str::<InfoJson>(&content)?;
-    println!("cargo:rustc-env=CS2_BUILD_NUMBER={}", info.build_number);
+    std::fs::write(to, content)
+    .expect("Write to file");
 
     Ok(())
 }
 
+fn build_number() -> Result<(), Box<dyn Error>> {
+    let content = reqwest::blocking::get("https://mirror.ghproxy.com/https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/info.json")
+        .unwrap_or_else(|_| panic!("Downloading info.json"))
+        .text()
+        .expect("Convert response to text");
+
+    let info = serde_json::from_str::<InfoJson>(&content)?;
+    println!("cargo:rustc-env=CS2_BUILD_NUMBER={}", info.build_number);
+	@@ -34,21 +36,20 @@ fn build_number() -> Result<(), Box<dyn Error>> {
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    download(
-        "https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client.dll.rs",
-        "./src/dma/cs2dumper/client_mod.rs",
-    )?;
 
     download(
-        "https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.rs",
-        "./src/dma/cs2dumper/offsets_mod.rs",
-    )?;
+        "https://mirror.ghproxy.com/https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client.dll.rs",
+        "./src/dma/cs2dumper/client_mod.rs"
+    ).expect("Failed to download build file \"client.dll.rs\"");
 
     download(
-        "https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/engine2.dll.rs",
-        "./src/dma/cs2dumper/engine2_mod.rs",
-    )?;
+        "https://mirror.ghproxy.com/https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.rs",
+        "./src/dma/cs2dumper/offsets_mod.rs"
+    ).expect("Failed to download build file \"offsets.rs\"");
+
+    download(
+        "https://mirror.ghproxy.com/https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/engine2.dll.rs",
+        "./src/dma/cs2dumper/engine2_mod.rs"
+    ).expect("Failed to download build file \"engine2.dll.rs\"");
 
     build_number()?;
 
-    EmitBuilder::builder()
-        .git_sha(true)
-        .git_commit_date()
-        .cargo_debug()
-        .cargo_target_triple()
-        .rustc_semver()
-        .rustc_llvm_version()
+	@@ -62,4 +63,4 @@ fn main() -> Result<(), Box<dyn Error>> {
         .emit()?;
 
     Ok(())
